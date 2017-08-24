98947111999561777d247b49b3a1038d16df2b7f
kishoresourav <31246164+kishoresourav@users.noreply.github.com>
2017-08-24 00:54:31 -0700

    Add files via upload

---
+++ Lab1_question1.cpp
@@ -0,0 +1,20 @@
+//C++ PROGRAM TO PERFORM INPUT/OUTPUT OF ALL BASIC DATA TYPES
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a=7;
+  char chr='s';
+  bool bl=true;
+  float ft=5.678;
+
+   cout <<"integer:"<<a<< endl;
+   cout <<"character:"<<chr<< endl;
+   cout <<"boolean:"<<bl<< endl;
+   cout <<"float:"<<ft<< endl;
+    return 0;
+}
+
+
