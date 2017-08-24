e368d33f762b8952a01a536f32ff09022dd08adf
kishoresourav <31246164+kishoresourav@users.noreply.github.com>
2017-08-24 00:54:57 -0700

    Add files via upload

---
+++ Lab1_question2.cpp.cpp
@@ -0,0 +1,14 @@
+//C++ PROGRAM TO ENTER TWO NUMBERS AND FIND THEIR SUM
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum of the numbers:"<<c;
+    return 0;
+}
